b218a2f0f60302fbd7f9cf65136fff8802f62889
Cristian Contreras <contreras.cristian994@gmail.com>
2022-11-01 23:27:37 -0400

    commit tree 91bd0b0c29bdbc4a8039fe607db16d36b2b2627c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName = 'bob';
+console.log("Hello!")
+
+function upperCaseCustomerName(){
+    console.log(customerName.toUpperCase());
+}
+upperCaseCustomerName();
